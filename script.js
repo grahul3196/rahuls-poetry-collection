04ead75ca329f7e08750fa5e14c2c6ce1e229ac0
grahul3196 <grahul3196@gmail.com>
2025-01-25 00:59:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -88,11 +88,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Dark mode toggle
     const darkModeToggle = document.getElementById('dark-mode-toggle');
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme) {
+        document.body.classList.add(currentTheme);
+    }
+
     darkModeToggle.addEventListener('click', () => {
         document.body.classList.toggle('dark-mode');
+        let theme = 'light';
+        if (document.body.classList.contains('dark-mode')) {
+            theme = 'dark-mode';
+        }
+        localStorage.setItem('theme', theme);
     });
 });
-
-
-
 
